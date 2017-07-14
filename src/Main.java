f516c032a658ab27f18f1003a04802163707049a
RedEis91 <imitationkrabs91@gmail.com>
2017-07-14 10:15:23 -0400

    fixed functionality, looping works

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,56 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 import java.util.Random;
 import java.util.Scanner;
 
 
-
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hey big baller, welcome to the casino!");
-        Random randGen = new Random();  // New random number generator
+        System.out.println("Hey big baller, welcome to the world series of dice!");
         Scanner scan = new Scanner(System.in);
         int sides = 1;
-        String cont = "yes";
-        do {
-            diceDiceBaby(sides, scan, randGen, cont);
-        } while (cont.contains("y") || cont.contains("Y"));
-        return;
-    }
-
-    public static void diceDiceBaby (int sides, Scanner scan, Random randGen, String cont) {
-        System.out.println("How many sides do you ");
+        int roll = 1;
+        boolean contin;
+        String cont = " ";
+        System.out.println("How many sides on your pair o' bones?");
         sides = scan.nextInt();
-        System.out.println(sides + " sided dice rolling...");
+        //clearing enter from scanner
+        scan.nextLine();
+        int sides2 = sides;
+        do {
+            contin = diceDiceBaby(sides, sides2, scan, cont, roll);
 
+        } while (contin);
+    }
+    public static boolean diceDiceBaby (int sides, int sides2, Scanner scan, String cont, int roll) {
+        Random randGen = new Random();  // New random number generator
+        System.out.println("A pair of " + sides + " sided dice rolling...");
+        System.out.println("...rolling...");
+        System.out.println("...rolling...");
         // randGen.nextInt(6) yields 0, 1, 2, 3, 4, or 5
         // so + 1 makes that 1, 2, 3, 4, 5, or 6
+        System.out.println(" ");
+        System.out.println(" ");
+        System.out.println("Roll " + roll + ":");
         System.out.println(randGen.nextInt((sides)) + 1);
-        System.out.println(randGen.nextInt((sides)) + 1);
-        System.out.println("Roll again?");
+        System.out.println(randGen.nextInt((sides2)) + 1);
+        if ((randGen.nextInt((sides)) + 1) == 1 && (randGen.nextInt((sides2)) + 1) == 1)
+        {
+            System.out.println("Snake eyes!");
+        }
+        System.out.print("Roll again? (y/n)");
+        System.out.println(cont);
         cont = scan.nextLine();
-
+        System.out.println(cont);
+        if (cont.contains("y") || cont.contains("Y"))
+        {
+            System.out.println("Let's go!");
+            System.out.println(" ");
+            return true;
+        } else
+            {
+                System.out.println("Thanks for playing!");
+                return false;
+            }
     }
 }
